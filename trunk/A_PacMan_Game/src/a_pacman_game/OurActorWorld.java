a54a6c702e5f308ab88272f669fbfa26d3e1bc1f
dfryer1193@gmail.com <dfryer1193@gmail.com@0cdd7824-fead-b70b-a334-aadc589883f7>
2011-05-13 16:34:30 +0000

    Fuck. YOU.

---
+++ trunk/A_PacMan_Game/src/a_pacman_game/OurActorWorld.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a_pacman_game;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Location;
+
+/**
+ *
+ * @author David Fryer
+ */
+public class OurActorWorld extends ActorWorld {
+
+    /**
+     * creates a new actor world, giving controls above the grid
+     */
+    public OurActorWorld() {
+        super();
+        setGrid(new BoundedGrid<Actor>(21, 19));
+        setMessage("Click 'Run' to play.\nUse the Arrow Keys to change"
+                + " direction. Clear the flowers to win.\nOh yeah, Don't get"
+                + " eaten!");
+    }
+
+    /**
+     * overrides the clicks within the grid, disabling the menus.
+     * @param loc
+     * @return
+     */
+    @Override
+    public boolean locationClicked(Location loc) {
+        return true;
+    }
+
+    /**
+     * handles keys being pressed. if it is a directional key,
+     * it finds pacman, otherwise, it lets the guiController class handle
+     * the key
+     * @param description
+     * @param loc
+     * @return
+     */
+    @Override
+    public boolean keyPressed(String description, Location loc) {
+        if (description.equalsIgnoreCase("UP")
+                || description.equalsIgnoreCase("DOWN")
+                || description.equalsIgnoreCase("RIGHT")
+                || description.equalsIgnoreCase("LEFT")) {
+            for (Location l : getGrid().getOccupiedLocations()) {
+                if (getGrid().get(l) instanceof pacMan) {
+                    ((pacMan) getGrid().get(l)).step(description);
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ trunk/A_PacMan_Game/src/a_pacman_game/pacManMain.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package a_pacman_game;
+
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+import java.awt.Color;
+
+/**
+ *
+ * @author David Fryer
+ */
+public class pacManMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        OurActorWorld pacWorld = new OurActorWorld();
+        pacMan pacman = new pacMan();
+        pacWorld.add(new Location(11, 9), pacman);
+        pacWorld.show();
+        makeBounds(pacWorld);
+        for (int r = 0; r < pacWorld.getGrid().getNumRows(); r++) {
+            for (int c = 0; c < pacWorld.getGrid().getNumCols(); c++) {
+                Location l = new Location(r, c);
+                if (pacWorld.getGrid().get(l) == null) {
+                    pacWorld.add(l, new Flower(Color.YELLOW));
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds the boundaries to the world.
+     * @param pacWorld
+     */
+    private static void makeBounds(World pacWorld){
+        for (int x = 0; x < pacWorld.getGrid().getNumRows(); x++) {
+            for (int y = 0; y < pacWorld.getGrid().getNumCols(); y++) {
+                Location l = new Location(x, y);
+                if (x == 0 || x == pacWorld.getGrid().getNumRows() - 1) {
+                    pacWorld.add(l, new Rock());
+                } else {
+                    if (x != 9 && (y == 0 || y == pacWorld.getGrid().
+                            getNumCols() - 1)) {
+                        pacWorld.add(l, new Rock());
+                    }
+                    if (x < 3 && y == 9) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 2 && ((y > 1 && y < 4) || (y > 4 && y < 8)
+                            || (y > 10 && y < 14) || (y > 14 && y < 17))) {
+                        pacWorld.add(l, new Rock());
+                    } else if ((x == 4 || x == 12) && ((y > 1 && y < 4)
+                            || (y == 5) || (y > 6 && y < 12) || (y == 13)
+                            || (y > 14 && y < 17))) {
+                        pacWorld.add(l, new Rock());
+                    } else if ((x == 5 || x == 17) && (y == 5 || y == 9
+                            || y == 13)) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 6 && ((y < 4) || (y > 4 && y < 8)
+                            || (y == 9) || (y > 10 && y < 14) || y > 14)) {
+                        pacWorld.add(l, new Rock());
+                    } else if (((x > 6 && x < 12) && x != 9) && ((y < 4)
+                            || (y == 5) || (y == 13) || (y > 14))) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 13 && y == 9) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 14 && ((y > 1 && y < 4) || (y > 4 && y < 8)
+                            || (y == 9) || (y > 10 && y < 14)
+                            || (y > 14 && y < 17))) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 15 && (y == 3 || y == 15)) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 16 && ((y == 1) || (y == 3) || (y == 5)
+                            || (y > 6 && y < 12) || (y == 13) || (y == 15)
+                            || (y == 17))) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 18 && ((y > 1 && y < 8) || (y == 9)
+                            || (y > 10 && y < 17))) {
+                        pacWorld.add(l, new Rock());
+                    }
+                    if ((x == 8 || x == 10) && (y > 6 && y < 12)) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 9 && (y == 7 || y == 11)) {
+                        pacWorld.add(l, new Rock());
+                    } else if (x == 12 && (y == 1 || y == 17)) {
+                        pacWorld.add(l, new Rock());
+                    }
+                }
+            }
+        }
+    }
+
+}
